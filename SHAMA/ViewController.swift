eb318b099f5675d4431bff158533109d658b0da8
farid <engfaridooo@gmail.com>
2019-07-31 07:39:28 -0700

    Initial Commit

---
+++ SHAMA/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SHAMA
+//
+//  Created by Farido on 7/31/19.
+//  Copyright © 2019 Farido. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
